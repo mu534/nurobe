e00d2b3ad450840e16aaa782cea1c245e9d5f5e5
mudasir najimudin <mudassirnajimuddin@gmail.com>
2026-02-14 17:10:34 +0300

    create room service

---
+++ src/services/rooms.service.ts
@@ -0,0 +1,37 @@
+import { api } from "./api";
+
+export interface Room {
+  id: number;
+  name: string;
+  type: string;
+  price: number;
+  maxGuests: number;
+  size: string;
+  bedType: string;
+  available: boolean;
+}
+
+export const getRooms = async () => {
+  const response = await api.get<Room[]>("/rooms");
+  return response.data;
+};
+
+export const getRoomById = async (id: number) => {
+  const response = await api.get<Room>(`/rooms/${id}`);
+  return response.data;
+};
+
+export const createRoom = async (data: Omit<Room, "id">) => {
+  const response = await api.post("/rooms", data);
+  return response.data;
+};
+
+export const updateRoom = async (id: number, data: Partial<Room>) => {
+  const response = await api.put(`/rooms/${id}`, data);
+  return response.data;
+};
+
+export const deleteRoom = async (id: number) => {
+  const response = await api.delete(`/rooms/${id}`);
+  return response.data;
+};
